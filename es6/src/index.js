e2dbca6fb66384148ea4fc14981bc88d3ce5c990
shiminghua <shiminghua2008@qq.com>
2018-03-16 18:44:28 +0800

    moment

--- es6/src/index.js
+++ es6/src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 // import './symbol/index.js';
-import './date';
+// import './date';
+import './moment/month';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
-console.log(React);
+// console.log(React);
