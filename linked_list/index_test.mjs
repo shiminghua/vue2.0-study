98883bb4ed29397e22173d9c5a2303bcb3710921
shiminghua <shiminghua2008@qq.com>
2020-06-03 18:50:29 +0800

    linked list

---
+++ linked_list/index_test.mjs
@@ -0,0 +1,9 @@
+import LinkedList from './linked_list.mjs';
+
+let linkedList = new LinkedList();
+
+linkedList.push(1);
+linkedList.push(2);
+// linkedList.push(3);
+
+console.log(111111, linkedList);
