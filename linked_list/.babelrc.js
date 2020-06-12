fe37188e019a805376f6553dfbefebee649d46fe
shiminghua <shiminghua2008@qq.com>
2020-06-12 14:34:42 +0800

    支持私有方法

--- linked_list/.babelrc.js
+++ linked_list/.babelrc.js
@@ -4,7 +4,7 @@ module.exports = {
   ],
   plugins: [
     "@babel/plugin-proposal-class-properties", // 私有属性
-    // "@babel/plugin-proposal-function-bind",
     "@babel/plugin-proposal-private-methods", // 私有方法
+    // "@babel/plugin-proposal-function-bind",
   ]
 };
